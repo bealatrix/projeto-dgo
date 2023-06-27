f6e3c2704f78fe9924a9349e46b63effdd2ecfab
Beatriz Vitoria <beatrizvitoria519@gmail.com>
2023-06-27 14:36:16 -0400

    Criando Componentes

---
+++ services/src/api/components/base/base.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+
+export class BaseController {
+  public index(req: Request, res: Response) {
+    res.status(200).json({ message: "Api running...." })
+  }
+
+  public info(req: Request, res: Response) {
+    res.status(200).json({
+      name: "API REST - DGO",
+      mode: "development",
+      version: "1.0.0",
+    })
+  }
+}
